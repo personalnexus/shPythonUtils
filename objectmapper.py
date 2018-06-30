69abe2a3c8934829cb247b26927c10a2a2cd960e
StefanH <personalnexus@users.noreply.github.com>
2018-06-30 18:43:52 +0200

    Add files via upload
    Helper functions to populate objects' attributes with values from string->string dictionaries

---
+++ objectmapper.py
@@ -0,0 +1,31 @@
+from typing import Callable, TypeVar
+
+T = TypeVar('T')
+
+
+def create_mapped_object(target_type: Callable[[], T], value_dictionary: dict) -> T:
+    """
+    Creates an instance of target_type and calls map_object on it
+    :param target_type: Callable[[], T]
+    :param value_dictionary: dict
+    :return: an instance of target_type
+    """
+    result = target_type()
+    map_object(result, value_dictionary)
+    return result
+
+
+def map_object(target_object: T, value_dictionary: dict) -> None:
+    """
+    Looks for attributes on target_object that map to keys found in value_dictionary. If the attribute on target_object
+    is not None the value from value_dictionary is converted to the type of that attribute
+    :param target_object: T
+    :param value_dictionary: dict
+    """
+    for attribute_name, current_attribute_value in target_object.__dict__.items():
+        if attribute_name in value_dictionary:
+            if current_attribute_value is not None:
+                new_value = current_attribute_value.__class__(value_dictionary[attribute_name])
+            else:
+                new_value = value_dictionary[attribute_name]
+            target_object.__setattr__(attribute_name, new_value)
---
+++ test/objectmapper_test.py
@@ -0,0 +1,53 @@
+import unittest
+import objectmapper
+
+
+class MapperTest(unittest.TestCase):
+    def test_create_mapped_object(self):
+        mapped_object = objectmapper.create_mapped_object(MyMappedObject1,
+                                                          {'value1': '12',
+                                                           'value2': 'bye',
+                                                           'value3': 'nothing',
+                                                           'value5': '2.5'})
+        self.assertIsTypedValue(12, mapped_object.value1)
+        self.assertIsTypedValue('bye', mapped_object.value2)
+        self.assertIsTypedValue(1.2, mapped_object.value4)
+        self.assertIsTypedValue(2.5, mapped_object.value5)
+
+    def test_map_object_with_no_match(self):
+        mapped_object = MyMappedObject2()
+        objectmapper.map_object(mapped_object, {'value3': '12'})
+        self.assertIsNone(mapped_object.value1)
+        self.assertRaises(TypeError, lambda: mapped_object.value3)
+
+    def test_map_object_with_match(self):
+        mapped_object = MyMappedObject2()
+        objectmapper.map_object(mapped_object, {'value1': '12'})
+        self.assertIsTypedValue('12', mapped_object.value1)
+
+    def assertIsTypedValue(self, expected_value, actual_value):
+        self.assertEqual(expected_value, actual_value)
+        self.assertIsInstance(actual_value, type(expected_value))
+
+
+class MyMappedObject1(object):
+    def __init__(self):
+        super(MyMappedObject1, self).__init__()
+        self.value1 = 1
+        self.value2 = 'hello'
+        self.value4 = 1.2
+        self.value5 = 0.0
+
+
+class MyMappedObject2(object):
+    def __init__(self):
+        super(MyMappedObject2, self).__init__()
+        self.value1 = None
+
+    @property
+    def value3(self):
+        return 3 * self.value1
+
+
+if __name__ == '__main__':
+    unittest.main()
